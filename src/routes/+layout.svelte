b5a8e52599ac76ca9a69aef2c9dbf750c6e0d03f
shreejan shrestha <shr.shreejan@gmail.com>
2024-07-22 18:38:01 +0545

    Added css for base template layout

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,19 @@
 <script lang="ts">
 	import '../app.css';
-	let children = $props();
+	let { children } = $props();
 </script>
 
-{@render children()}
+<main>
+	<div class="main-container flex w-full">
+		<nav class="sidebar flex flex-col gap-4 py-4 pl-4">
+			<div class="text-5xl">Components.</div>
+			<div class="component-list flex flex-col">
+				<div class="text-xl">Component 1</div>
+				<div class="text-xl">Component 2</div>
+			</div>
+		</nav>
+		<div class="w-full">
+			{@render children()}
+		</div>
+	</div>
+</main>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,1 +1,6 @@
-<main>Components.</main>
+<main>
+	<div class="primary-container flex min-h-screen flex-col items-center justify-center gap-4 p-4">
+		<div class="flex w-full flex-grow items-center justify-center border-2">Container 1</div>
+		<div class="flex w-full flex-grow items-center justify-center border-2">Container 2</div>
+	</div>
+</main>
