5e691b9958fd66778fc5877365df2dd6db1e909b
shreejan shrestha <shr.shreejan@gmail.com>
2024-07-24 20:26:24 +0545

    UI improvements

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -7,7 +7,7 @@
 <main class="flex h-screen justify-center">
 	<div class="flex h-full w-[80vw] flex-col gap-4 p-4">
 		<div class="flex h-1/2 flex-grow items-center justify-center border-2">
-			<CustomScrollbar />
+			<InfiniteScroller />
 		</div>
 		<div class="flex h-1/2 flex-grow items-center justify-center border-2">Container 2</div>
 	</div>
