3dcf7ae2b117564a59492bf9359de8a324643cf4
shreejan shrestha <shr.shreejan@gmail.com>
2024-07-28 19:27:10 +0545

    UI improvements

--- src/lib/templates/Base.svelte
+++ src/lib/templates/Base.svelte
@@ -2,6 +2,16 @@
 	import type { ComponentInfo } from '$lib/types/types';
 	import { ArrowLeft, Code, Expand, Minimize2 } from 'lucide-svelte';
 	let { component, children }: { component: ComponentInfo; children: any } = $props();
+
+	let isExpanded: boolean = $state(false);
+
+	function toggleExpand() {
+		isExpanded = !isExpanded;
+	}
+
+	function goBack() {
+		window.history.back();
+	}
 </script>
 
 <main class="flex h-screen justify-center">
@@ -9,23 +19,36 @@
 		<div class="flex h-2/3 flex-grow items-center justify-center border-2">
 			{@render children()}
 		</div>
-		<div class="flex h-1/3 flex-grow flex-col border-2 p-4">
-			<div class="flex justify-between">
-				<h1 class="text-7xl">{component.componentName}</h1>
-				<div class="flex gap-4 p-4">
-					<button class="border-2 p-2">
-						<ArrowLeft />
-					</button>
-					<button class="border-2 p-2">
-						<Code />
-					</button>
-					<button class="border-2 p-2">
-						<Expand />
-					</button>
+		{#if !isExpanded}
+			<div class="flex h-1/3 flex-grow flex-col border-2 p-4">
+				<div class="flex justify-between">
+					<h1 class="text-7xl">{component.componentName}</h1>
+					<div class="flex gap-4 p-4">
+						<button class="border-2 bg-neutral-50 p-2 dark:bg-slate-900" onclick={goBack}>
+							<ArrowLeft />
+						</button>
+						<a
+							class="border-2 bg-neutral-50 p-2 dark:bg-slate-900"
+							href={component.url}
+							target="_blank"
+						>
+							<Code />
+						</a>
+						<button class="border-2 bg-neutral-50 p-2 dark:bg-slate-900" onclick={toggleExpand}>
+							<Expand />
+						</button>
+					</div>
 				</div>
+				<p class="text-2xl">{component.description}</p>
 			</div>
-			<p class="text-2xl">{component.description}</p>
-		</div>
+		{/if}
 	</div>
-	<button class="absolute right-4 top-4 border-2 bg-white p-2"><Minimize2 /></button>
+	{#if isExpanded}
+		<button
+			class="absolute right-4 top-4 border-2 bg-neutral-50 p-2 dark:bg-slate-900"
+			onclick={toggleExpand}
+		>
+			<Minimize2 />
+		</button>
+	{/if}
 </main>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,9 +1,10 @@
 <script lang="ts">
 	import '../app.css';
 	import componentData from '$lib/data/components.json';
-	import { Menu, X, Moon } from 'lucide-svelte';
-	let { children } = $props();
+	import { Menu, X, Sun, Moon } from 'lucide-svelte';
+	import { ModeWatcher, toggleMode } from 'mode-watcher';
 
+	let { children } = $props();
 	let isSidebarOpen = $state(false);
 
 	function toggleSidebar() {
@@ -14,10 +15,10 @@
 <main>
 	<div class="main-container flex w-full">
 		<nav
-			class={`sidebar fixed z-50 flex h-full flex-col gap-4 border-r-2 bg-white p-4 transition-transform duration-300 ease-in-out ${isSidebarOpen ? 'translate-x-0' : '-translate-x-full'}`}
+			class={`sidebar fixed z-50 flex h-full flex-col gap-4 border-r-2 bg-neutral-50 p-4 transition-transform duration-300 ease-in-out dark:bg-slate-900 ${isSidebarOpen ? 'translate-x-0' : '-translate-x-full'}`}
 		>
 			<div class="flex items-end gap-8">
-				<div class="text-5xl">Components.</div>
+				<a href="/" class="text-5xl">Components.</a>
 
 				<button class="rounded-full border-2 p-2" onclick={toggleSidebar}>
 					<X />
@@ -36,16 +37,18 @@
 			{@render children()}
 		</div>
 		<div class="absolute bottom-4 right-4 flex gap-2 p-2">
-			<button class="border-2 bg-white p-2" onclick={toggleSidebar}>
+			<button class="border-2 bg-neutral-50 p-2 dark:bg-slate-900" onclick={toggleSidebar}>
 				{#if isSidebarOpen}
 					<X />
 				{:else}
 					<Menu />
 				{/if}
 			</button>
-			<button class="border-2 p-2">
-				<Moon />
+			<button class="border-2 bg-neutral-50 p-2 dark:bg-slate-900" onclick={toggleMode}>
+				<Sun class="absolute rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
+				<Moon class="rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
 			</button>
 		</div>
 	</div>
 </main>
+<ModeWatcher />
